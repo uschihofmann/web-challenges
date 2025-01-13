6004c6ff51286b9fc225ade9872da11f711e78c9
Uschi <uschihofmann@gmx.de>
2025-01-13 15:51:18 +0100

    Aufgabe hochgeladen

---
+++ js-conditions-and-booleans_statements/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+
+//const receivedPassword = "password1234";
+
+const receivedPassword = "wrong1234";
+
+if (receivedPassword) {
+ receivedPassword= SUPER_SECRET_PASSWORD;
+ console.log(Welcome! You are logged in as Brunhilde1984.);
+} else {}
+console.log("Du kommst hier nicht rein!");
+
+// // Part 2: Even / Odd
+// //const number = 6;
+
+// // Part 3: Hotdogs
+// const numberOfHotdogs = 42;
+
+// // Part 4: Daytime
+// const currentHour = 12;
+
+// const statement = "";
+
+// console.log(statement);
+
+// // Part 5: Greeting
+// const userName = "Archibald";
+
+// const greeting = "Hello " + "//enter your code here" + "!";
+
+// console.log(greeting);
