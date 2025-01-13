e8fd918b334015e2c66bbca3e7cb35b08c5adbf7
Uschi <uschihofmann@gmx.de>
2025-01-13 16:59:01 +0100

    Aufgaben hinzugefügt

---
+++ js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
